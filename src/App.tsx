b8022132a3ba7edd6e39c0849c4bf84db4ca6704
isabelxklee <hi@kleetime.com>
2024-02-14 23:39:12 -0500

    updated readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable array-callback-return */
+import React, { useMemo } from "react";
+import "./App.css";
 
 function App() {
+  const timeZoneList = useMemo(() => {
+    const unique: any[] = [];
+
+    Intl.supportedValuesOf("timeZone").map((timeZone) => {
+      const obj = {
+        offset: {},
+        timeZone: "",
+      };
+
+      const offset = new Intl.DateTimeFormat("en", {
+        timeZone: timeZone,
+        timeZoneName: "shortOffset",
+      }).formatToParts();
+
+      const timeZoneAbbrivation = new Intl.DateTimeFormat("en", {
+        timeZone: timeZone,
+        timeZoneName: "long",
+      }).formatToParts();
+
+      obj["offset"] = offset[6].value;
+      obj["timeZone"] = timeZoneAbbrivation[6].value;
+
+      const result = unique.filter((x: any) => x.offset === obj.offset);
+
+      if (result.length === 0) {
+        unique.push(obj);
+      }
+    });
+
+    return unique;
+  }, []);
+
+  const sort = useMemo(() => {
+    return timeZoneList.sort((a, b) => {
+      const re = /^\GMT([+-]\d{1,2}):?(\d{1,2})?\.*$/;
+      let bOffset = 0;
+
+      if (b["offset"] !== "GMT") {
+        bOffset = parseFloat(b["offset"].replace(re, "$1.$2"));
+      }
+
+      const aOffset = parseFloat(a["offset"].replace(re, "$1.$2"));
+
+      return aOffset < bOffset ? -1 : aOffset > bOffset ? 1 : 0;
+    });
+  }, [timeZoneList]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {sort.map((item, index) => (
+        <div key={index}>
+          <p>
+            {item.offset} {item.timeZone}
+          </p>
+        </div>
+      ))}
     </div>
   );
 }
